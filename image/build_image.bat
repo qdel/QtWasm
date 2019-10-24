33fb82fc4b578f72aa3144b10a29c21305ae1d8d
Fredrik Orderud <forderud@gmail.com>
2019-10-24 21:18:02 +0200

    Use a separate "debug" tag for debug builds of Qt.

--- image/build_image.bat
+++ image/build_image.bat
@@ -2,9 +2,9 @@
 ::set DOCKER_BUILDKIT=false
 
 :: Build container
-docker build --file Dockerfile --tag=forderud/qtwasm:latest .
+docker build --file Dockerfile --tag=forderud/qtwasm:debug .
 
 IF %ERRORLEVEL% EQU 0 (
   :: Push to dockerhub
-  docker push forderud/qtwasm:latest
+  docker push forderud/qtwasm:debug
 )
