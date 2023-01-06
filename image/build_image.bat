19d7c27fa425f6252eaf688dc583e63b7fbda986
Fredrik Orderud <forderud@gmail.com>
2023-01-06 14:59:28 +0100

    Switch to "qt6.5" as image tag.

--- image/build_image.bat
+++ image/build_image.bat
@@ -2,9 +2,9 @@
 ::set DOCKER_BUILDKIT=false
 
 :: Build container
-docker build --file Dockerfile --tag=forderud/qtwasm:latest .
+docker build --file Dockerfile --tag=forderud/qtwasm:qt6.5 .
 
 IF %ERRORLEVEL% EQU 0 (
   :: Push to dockerhub
-  docker push forderud/qtwasm:latest
+  docker push forderud/qtwasm:qt6.5
 )
